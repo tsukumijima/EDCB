1f484f715e5acc2259852816865c7b7b0986468b
tkntrec <Tokinata@gmail.com>
2017-10-01 17:51:27 +0900

    文字列置換でサロゲートもキャッシュする

--- EpgTimer/EpgTimer/Common/ItemFontCache.cs
+++ EpgTimer/EpgTimer/Common/ItemFontCache.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Media;
@@ -32,21 +33,51 @@ namespace EpgTimer
         }
         public class GlyphTypeSet
         {
-            public readonly GlyphTypeface Type = null;
-            public readonly ushort[] indexCache = null;
-            public readonly float[] widthCache = null;
+            public readonly GlyphTypeface Type ;
+            public readonly Dictionary<int, ushort> hitDic = new Dictionary<int, ushort>();
+            public readonly int[] indexCache;
+            public readonly float[] widthCache;
             public GlyphTypeSet(FontFamily fontfamily, bool isBold)
             {
                 new Typeface(fontfamily, FontStyles.Normal, isBold ? FontWeights.Bold : FontWeights.Normal, FontStretches.Normal).TryGetGlyphTypeface(out Type);
-                indexCache = Type == null ? null : new ushort[ushort.MaxValue + 1];
+                indexCache = Type == null ? null : Enumerable.Repeat<int>(-1, ushort.MaxValue + 1).ToArray();
                 widthCache = Type == null ? null : new float[ushort.MaxValue + 1];
+            }
+            public ushort GlyphIndex(int key)
+            {
+                ushort glyphIndex;
+                if (key < indexCache.Length)
+                {
+                    if (indexCache[key] >= 0)
+                    {
+                        return (ushort)indexCache[key];
+                    }
+                    Type.CharacterToGlyphMap.TryGetValue(key, out glyphIndex);
+                    indexCache[key] = glyphIndex;
+                }
+                else
+                {
+                    if (hitDic.TryGetValue(key, out glyphIndex) == true)
+                    {
+                        return glyphIndex;
+                    }
+                    Type.CharacterToGlyphMap.TryGetValue(key, out glyphIndex);
+                    hitDic.Add(key, glyphIndex);
+                }
+
+                double glyphWidth;
+                Type.AdvanceWidths.TryGetValue(glyphIndex, out glyphWidth);
+                widthCache[glyphIndex] = (float)glyphWidth;
+                return glyphIndex;
             }
         }
     }
     public class ItemFont
     {
-        public readonly GlyphTypeface[] GlyphType = null;
-        private readonly ItemFontCache.GlyphTypeSet[] cacheSet = null;
+        public readonly GlyphTypeface[] GlyphType;
+        private readonly ItemFontCache.GlyphTypeSet[] cacheSet;
+        private readonly short[] fontCache = Enumerable.Repeat<short>(-1, ushort.MaxValue + 1).ToArray();
+        private readonly Dictionary<int, Tuple<int, ushort>> fontDic = new Dictionary<int, Tuple<int, ushort>>();
         public ItemFont(string familyName, bool isBold)
         {
             cacheSet = familyName.Split(new[] { ',' })
@@ -61,37 +92,43 @@ namespace EpgTimer
         public double GlyphWidth(string line, ref int n, out ushort glyphIndex, out int fontIndex)
         {
             int key = line[n];
-            for (fontIndex = 0; fontIndex < cacheSet.Length; fontIndex++)
+            fontIndex = fontCache[key];
+            if (fontIndex >= 0)
             {
-                glyphIndex = cacheSet[fontIndex].indexCache[key];
-                if (glyphIndex != 0) return cacheSet[fontIndex].widthCache[glyphIndex];
-            }
-
-            if (char.IsSurrogatePair(line, n))
-            {
-                key = char.ConvertToUtf32(line, n++);
-            }
-            else if (char.IsSurrogate((char)key))//不完全なサロゲートペアの混入対策
-            {
-                key = 0;
+                glyphIndex = (ushort)cacheSet[fontIndex].indexCache[key];
+                return cacheSet[fontIndex].widthCache[glyphIndex];
             }
 
             glyphIndex = 0;
+            if (char.IsSurrogatePair(line, n))
+            {
+                key = char.ConvertToUtf32(line, n++);
+                Tuple<int, ushort> keyData;
+                if (fontDic.TryGetValue(key, out keyData) == true)
+                {
+                    fontIndex = keyData.Item1;
+                    glyphIndex = keyData.Item2;
+                    return cacheSet[fontIndex].widthCache[glyphIndex];
+                }
+            }
+            else if (char.IsSurrogate((char)key)) key = 0;//可能性は低いが一応不完全なサロゲートペアの混入対策
+
             for (fontIndex = 0; fontIndex < cacheSet.Length; fontIndex++)
             {
-                if (GlyphType[fontIndex].CharacterToGlyphMap.TryGetValue(key, out glyphIndex) == true) break;
+                glyphIndex = cacheSet[fontIndex].GlyphIndex(key);
+                if (glyphIndex != 0) break;
             }
             if (fontIndex == cacheSet.Length) fontIndex = 0;
 
-            double glyphWidth;
-            GlyphType[fontIndex].AdvanceWidths.TryGetValue(glyphIndex, out glyphWidth);
             if (key < cacheSet[fontIndex].indexCache.Length)
             {
-                cacheSet[fontIndex].indexCache[key] = glyphIndex;
-                cacheSet[fontIndex].widthCache[glyphIndex] = (float)glyphWidth;
+                fontCache[key] = (byte)fontIndex;
             }
-
-            return (float)glyphWidth;
+            else
+            {
+                fontDic[key] = new Tuple<int, ushort>(fontIndex, glyphIndex);
+            }
+            return cacheSet[fontIndex].widthCache[glyphIndex];
         }
     }
 }
