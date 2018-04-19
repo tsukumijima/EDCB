7ea3a9909b5178a224a7fe7c97bcde7b41ad21b0
tkntrec <Tokinata@gmail.com>
2018-04-20 00:21:49 +0900

    TSIDの変更を設定画面で検出する( xtne6f@562f565 )
    設定画面がモードレスなので一部変更して適用。

--- EpgTimer/EpgTimer/Setting/SettingWindow.xaml.cs
+++ EpgTimer/EpgTimer/Setting/SettingWindow.xaml.cs
@@ -50,15 +50,16 @@ namespace EpgTimer
             button_OK.Click += (sender, e) => { this.Close(); Apply(); };
             button_cancel.Click += (sender, e) => this.Close();
 
-            LoadSetting();
+            LoadSetting(CommonManager.Instance.IsConnected == true);
             SetMode(mode, param);
         }
 
-        public void LoadSetting()
+        public void LoadSetting(bool init = false)
         {
             try
             {
                 DataContext = Settings.Instance.DeepCloneStaticSettings();
+                if (init == true) CheckServiceSettings((Settings)DataContext, false);
                 setBasicView.LoadSetting();
                 setAppView.LoadSetting();
                 setEpgView.LoadSetting();
@@ -110,6 +111,75 @@ namespace EpgTimer
             {
                 MessageBox.Show(ex.Message + "\r\n" + ex.StackTrace);
                 MessageBox.Show("不正な入力値によるエラーのため、一部設定のみ更新されました。");
+            }
+        }
+
+        private static void CheckServiceSettings(Settings settings, bool apply)
+        {
+            //サービス一覧に見つからず、TSIDを無視すれば見つかるサービスがないか調べる
+            string searchDefault = "";
+            foreach(SearchPresetItem info in settings.SearchPresetList)
+            {
+                for (int i = 0; i < info.Data.serviceList.Count; i++)
+                {
+                    CheckService(apply, (ulong)info.Data.serviceList[i], newId => info.Data.serviceList[i] = (long)newId, ref searchDefault);
+                }
+            }
+
+            string viewService = "";
+            string searchKey = "";
+            foreach (CustomEpgTabInfo info in settings.CustomEpgTabList)
+            {
+                for (int i = 0; i < info.ViewServiceList.Count; i++)
+                {
+                    CheckService(apply, info.ViewServiceList[i], newId => info.ViewServiceList[i] = newId, ref viewService);
+                }
+                for (int i = 0; i < info.SearchKey.serviceList.Count; i++)
+                {
+                    CheckService(apply, (ulong)info.SearchKey.serviceList[i], newId => info.SearchKey.serviceList[i] = (long)newId, ref searchKey);
+                }
+            }
+
+            string iepgStation = "";
+            foreach (IEPGStationInfo info in settings.IEpgStationList)
+            {
+                CheckService(apply, info.Key, newId => info.Key = newId, ref iepgStation);
+            }
+
+            if (searchDefault != "" || viewService != "" || searchKey != "" || iepgStation != "")
+            {
+                if (MessageBox.Show("TransportStreamIDの変更を検出しました。\r\n\r\n" +
+                                    (searchDefault != "" ? "【検索プリセットのサービス絞り込み】\r\n" : "") + searchDefault +
+                                    (viewService != "" ? "【番組表の表示条件の表示サービス】\r\n" : "") + viewService +
+                                    (searchKey != "" ? "【番組表の表示条件の検索条件のサービス絞り込み】\r\n" : "") + searchKey +
+                                    (iepgStation != "" ? "【iEPG Ver.1の放送局リスト】\r\n" : "") + iepgStation +
+                                    "\r\n変更を設定ウィンドウに適用しますか？\r\n（「適用」ボタンを押すか、「OK」ボタンにより設定ウィンドウを閉じるまで変更は保存されません）",
+                                    "設定 - サービス情報変更の検出", MessageBoxButton.OKCancel, MessageBoxImage.Information, MessageBoxResult.Cancel) == MessageBoxResult.OK)
+                {
+                    CheckServiceSettings(settings, true);
+                }
+            }
+        }
+        private static void CheckService(bool apply, ulong id, Action<ulong> SetNewId, ref string log)
+        {
+            if (ChSet5.ChList.ContainsKey(id) == false)
+            {
+                ChSet5Item item = ChSet5.ChList.Values.FirstOrDefault(a => (a.Key & 0xFFFF0000FFFF) == (id & 0xFFFF0000FFFF));
+                if (item != null)
+                {
+                    if (apply)
+                    {
+                        SetNewId(item.Key);
+                    }
+                    else if (log.Count(c => c == '\n') < 5)
+                    {
+                        log += "  ID=0x" + id.ToString("X12") + " -> 0x" + item.Key.ToString("X12") + " (" + item.ServiceName + ")\r\n";
+                    }
+                    else if (log.EndsWith(".\r\n") == false)
+                    {
+                        log += "  ...\r\n";
+                    }
+                }
             }
         }
     }
